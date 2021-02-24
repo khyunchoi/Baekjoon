b895fd9a04865db513f2f3ec595dd2d856231003
khyunchoi <khyunchoi@naver.com>
2021-02-24 17:25:20 +0900

    1085번 무난한 난이도 배열정렬을 사용해보았다.

---
+++ src/Baekjoon/Main1085.java
@@ -0,0 +1,22 @@
+package Baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main1085 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int w = sc.nextInt();
+		int h = sc.nextInt();
+		sc.close();
+		
+		int[] arr = {x, y, w-x, h-y};
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0]);
+	}
+
+}
