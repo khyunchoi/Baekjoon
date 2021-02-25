c3278305c20d097b736245edf0658f5e28491eee
khyunchoi <khyunchoi@naver.com>
2021-02-25 20:32:15 +0900

    3053번 3분컷한듯

---
+++ src/Baekjoon/Main3053.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Main3053 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int R = sc.nextInt();
+		sc.close();
+		System.out.println(Math.pow(R, 2)*Math.PI);
+		System.out.println(Math.pow(R, 2)*2);
+	}
+
+}
