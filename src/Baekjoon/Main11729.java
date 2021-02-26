2a21c655235d23c525cf7eadbff2bf760253ddb9
khyunchoi <khyunchoi@naver.com>
2021-02-26 22:56:31 +0900

    StringBuilder를 통해 해결! 알고리즘에는 문제가 없었다.

--- src/Baekjoon/Main11729.java
+++ src/Baekjoon/Main11729.java
@@ -3,7 +3,8 @@ package Baekjoon;
 import java.util.Scanner;
 
 public class Main11729 {
-
+	public static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
@@ -11,14 +12,14 @@ public class Main11729 {
 		
 		System.out.println((int)Math.pow(2, N)-1);
 		hanoi(1, 3, 2, N);
-
+		System.out.println(sb);
 	}
 	
 	public static void hanoi(int a, int b, int c, int num) {
 		while (num >= 1) {
 			num--;
 			hanoi(a, c, b, num);
-			System.out.println(a + " " + b);
+			sb.append(a + " " + b + "\n");
 			hanoi(c, b, a, num);
 			break;
 		}
