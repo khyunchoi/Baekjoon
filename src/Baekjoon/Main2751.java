9069b18807cce189fde369799312a14bf1d25e45
khyunchoi <khyunchoi@naver.com>
2021-03-01 19:37:05 +0900

    merger sort를 잘못 사용한 줄 알고 계속 구글링을 진행하는데 틀린부분이 없는것 같아서 혹시 하고 출력을 BufferedWriter로 바꿧더니 정답처리됐다..

--- src/Baekjoon/Main2751.java
+++ src/Baekjoon/Main2751.java
@@ -1,8 +1,10 @@
 package Baekjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main2751 {
 
@@ -11,6 +13,7 @@ public class Main2751 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
 		src = new int[N];
 		tmp = new int[N];
@@ -21,9 +24,11 @@ public class Main2751 {
 		
 		mergeSort(0, N-1);
 		for (int i = 0; i < N; i++) {
-			System.out.println(src[i]);
+			bw.write(Integer.toString(src[i]));
+			bw.newLine();
 		}
-		
+		bw.flush();
+		bw.close();
 	}
 
 	public static void mergeSort(int start, int end) {
