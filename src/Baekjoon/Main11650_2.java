212a511e5370daf3fc977c34d515a22b5ab5e0dc
khyunchoi <khyunchoi@naver.com>
2021-03-03 22:10:22 +0900

    뭔가 병합정렬에서 오류가 생겼다.

---
+++ src/Baekjoon/Main11650_2.java
@@ -0,0 +1,66 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main11650_2 {
+	public static int arr[][];
+	public static int tmp[][];
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		arr = new int[N][2];
+		tmp = new int[N][2];
+		
+		for (int i = 0; i < N; i++) {
+			String tmp = br.readLine();
+			StringTokenizer st = new StringTokenizer(tmp);
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		mergeSort(0, N-1);
+		
+		System.out.println(Arrays.toString(arr[0]));
+		System.out.println(Arrays.toString(arr[1]));
+		System.out.println(Arrays.toString(arr[2]));
+		System.out.println(Arrays.toString(arr[3]));
+		System.out.println(Arrays.toString(arr[4]));
+	}
+
+	public static void mergeSort(int start, int end) {
+		if (start < end) {
+			int mid = (start + end) / 2;
+			mergeSort(start, mid);
+			mergeSort(mid+1, end);
+			int p = start;
+			int q = mid + 1;
+			int idx = p;
+			
+			while (p <= mid || q <= end) {
+				if (q > end || (p <= mid && arr[p][0] < arr[q][0])) {
+					tmp[idx][0] = arr[p][0];
+//					tmp[idx][1] = arr[p][1];
+					idx++;
+					p++;
+				} else {
+					tmp[idx][0] = arr[q][0];
+//					tmp[idx][1] = arr[q][1];
+					idx++;
+					q++;
+				}
+			}
+			
+			for (int i = start; i < end; i++) {
+				arr[i][0] = tmp[i][0];
+				arr[i][1] = tmp[i][1];
+			}
+		}
+	}
+}
