c02d0162fba82ba57292e7e95965ab3eba96ef47
khyunchoi <khyunchoi@naver.com>
2021-03-06 16:24:50 +0900

    이름 다시 수정

--- src/Baekjoon/Main11650.java
+++ src/Baekjoon/Main11650.java
@@ -8,60 +8,69 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main11650 {
-	public static int arr[][];
-	public static int tmp[][];
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
-		arr = new int[N][2];
-		tmp = new int[N][2];
-		
+		int[] arrx = new int[N];
+		int[] arry = new int[N];
+		// 두 배열에 각각 x값, y값 순서대로 대입
 		for (int i = 0; i < N; i++) {
-			String tmp = br.readLine();
-			StringTokenizer st = new StringTokenizer(tmp);
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
+			String coor = br.readLine();
+			StringTokenizer st = new StringTokenizer(coor);
+			arrx[i] = Integer.parseInt(st.nextToken());
+			arry[i] = Integer.parseInt(st.nextToken());
 		}
-		mergeSort(0, N-1);
-		
+		br.close();
+		// x값 기준으로 정렬하되 y값도 같이 움직일 수 있도록 함
+		int tmp = 0;
 		for (int i = 0; i < N; i++) {
-			bw.write(Integer.toString(arr[i][0]) + " ");
-			bw.write(Integer.toString(arr[i][1]));
+			for (int j = 0; j < N - 1 - i; j++) {
+				if (arrx[j] > arrx[j + 1]) {
+					tmp = arrx[j];
+					arrx[j] = arrx[j + 1];
+					arrx[j + 1] = tmp;
+					tmp = arry[j];
+					arry[j] = arry[j + 1];
+					arry[j + 1] = tmp;
+				}
+			}
+		}
+		// x값이 같을 경우 y값 정렬
+		for (int i = 0; i < N - 1; i++) {
+			if (arrx[i] == arrx[i + 1]) {
+				// 선택정렬
+				int j = i;
+				while (arrx[j] == arrx[j + 1]) {
+					int k = j;
+					while (arrx[k] == arrx[k + 1]) {
+						if (arry[j] > arry[k + 1]) {
+							tmp = arry[j];
+							arry[j] = arry[k + 1];
+							arry[k + 1] = tmp;
+						}
+						k++;
+						if (k == N - 1) {
+							break;
+						}
+					}
+					j++;
+					if (j == N - 1) {
+						break;
+					}
+				}
+
+			}
+		}
+		//출력
+		for (int i = 0; i < N; i++) {
+			bw.write(Integer.toString(arrx[i]) + " ");
+			bw.write(Integer.toString(arry[i]));
 			bw.newLine();
 		}
 		bw.flush();
 		bw.close();
 	}
 
-	public static void mergeSort(int start, int end) {
-		if (start < end) {
-			int mid = (start + end) / 2;
-			mergeSort(start, mid);
-			mergeSort(mid+1, end);
-			int p = start;
-			int q = mid + 1;
-			int idx = p;
-			
-			while (p <= mid || q <= end) {
-				if (q > end || (p <= mid && arr[p][1] < arr[q][1]) || (arr[p][1] == arr[q][1] && arr[p][0] < arr[q][0])) {
-					tmp[idx][0] = arr[p][0];
-					tmp[idx][1] = arr[p][1];
-					idx++;
-					p++;
-				} else {
-					tmp[idx][0] = arr[q][0];
-					tmp[idx][1] = arr[q][1];
-					idx++;
-					q++;
-				}
-			}
-			
-			for (int i = start; i <= end; i++) {
-				arr[i][0] = tmp[i][0];
-				arr[i][1] = tmp[i][1];
-			}
-		}
-	}
 }
