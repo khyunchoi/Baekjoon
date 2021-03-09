b6c0b50ccd92abe20b3b02fc2efd82a641945b26
khyunchoi <khyunchoi@naver.com>
2021-03-09 17:00:27 +0900

    굉장히 군더더기 없이 잘 짰다고 생각했는데 시간초과..?

---
+++ src/Baekjoon/Main14889.java
@@ -0,0 +1,66 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main14889 {
+	public static int[][] arr;
+	public static boolean[] isTrue;
+	public static int N;
+	public static int Min = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+		isTrue = new boolean[N];
+		for (int i = 0; i < N; i++) {
+			String tmp = br.readLine();
+			StringTokenizer st = new StringTokenizer(tmp);
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		br.close();
+
+		dfs(0);
+
+		System.out.println(Min);
+
+	}
+
+	// 백트래킹으로 팀 나누고 능력치 차이 최솟값 출력
+	public static void dfs(int depth) {
+		if (depth == N / 2) {
+			Min = Math.min(teamDiff(isTrue), Min);
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (!isTrue[i]) {
+				isTrue[i] = true;
+				dfs(depth + 1);
+				isTrue[i] = false;
+			}
+		}
+
+	}
+
+	// 팀 능력치 차이 리턴
+	public static int teamDiff(boolean[] isTrue) {
+		int sum1 = 0, sum2 = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (isTrue[i] && isTrue[j]) {
+					sum1 += arr[i][j];
+				} else if (!isTrue[i] && !isTrue[j]) {
+					sum2 += arr[i][j];
+				}
+			}
+		}
+
+		return Math.abs(sum1 - sum2);
+	}
+}
