09031e368b95a6bb984bc65d0f1977060b1fb030
khyunchoi <khyunchoi@naver.com>
2021-03-09 19:40:16 +0900

    동적계획법은 문제를 쪼개서 풀때 반복되는 호출을 줄이는 방법이다.

--- src/Baekjoon/Main1003.java
+++ src/Baekjoon/Main1003.java
@@ -5,15 +5,21 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main1003 {
+	public static int[] fibo;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int T = Integer.parseInt(br.readLine());
 		int[] arr = new int[T];
-
+		fibo = new int[41];
+		Arrays.fill(fibo, -1);
+		fibo[0] = 0;
+		fibo[1] = 1;
+		
 		for (int i = 0; i < T; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
@@ -32,12 +38,9 @@ public class Main1003 {
 	}
 
 	public static int fibonacci(int n) {
-		if (n == 0) {
-			return 0;
-		} else if (n == 1) {
-			return 1;
-		} else {
-			return fibonacci(n - 1) + fibonacci(n - 2);
+		if (fibo[n] == -1) {
+			fibo[n] = fibonacci(n - 1) + fibonacci(n - 2);
 		}
+		return fibo[n];
 	}
 }
