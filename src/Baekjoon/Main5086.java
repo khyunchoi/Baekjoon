ea7b4fcc2a58e981a030562032af9a423f0e8882
khyunchoi <khyunchoi@naver.com>
2021-03-24 16:33:21 +0900

    기초적인 문제

---
+++ src/Baekjoon/Main5086.java
@@ -0,0 +1,29 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Main5086 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if (a == 0 && b == 0) {
+				break;
+			}
+			
+			if (a % b == 0) {
+				System.out.println("multiple");
+			} else if (b % a == 0) {
+				System.out.println("factor");
+			} else {
+				System.out.println("neither");
+			}
+			
+		}
+
+	}
+
+}
