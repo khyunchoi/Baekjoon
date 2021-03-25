8b0810dc7526e201d1475ad25443e81e7fc11932
khyunchoi <khyunchoi@naver.com>
2021-03-25 16:53:08 +0900

    전 문제도 유클리드 호제법을 이용했기 때문에 똑같은 문제.

---
+++ src/Baekjoon/Main1934.java
@@ -0,0 +1,31 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Main1934 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int i = 0; i < T; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a * b / gcd(a, b));
+		}
+		sc.close();
+	}
+
+	public static int gcd(int a, int b) {
+		while (a > 0) {
+			if (a < b) {
+				int tmp = a;
+				a = b;
+				b = tmp;
+			}
+			a %= b;
+		}
+		return b;
+	}
+
+}
