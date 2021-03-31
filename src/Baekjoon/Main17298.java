9005a54374e79b6d26bfe49bbb253fd9102acd1f
khyunchoi <khyunchoi@naver.com>
2021-03-31 17:09:38 +0900

    BufferedReader와 StringBuilder를 이용했는데 시간초과가 난걸로 봐서 알고리즘 자체를 바꿔야할듯

--- src/Baekjoon/Main17298.java
+++ src/Baekjoon/Main17298.java
@@ -1,34 +1,40 @@
 package Baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main17298 {
 	public static int[] arr;
 	public static Stack<Integer> stack = new Stack<>();
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		arr = new int[N];
-		
+		String tmp = br.readLine();
+		StringTokenizer st = new StringTokenizer(tmp);
+
 		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		sc.close();
-		
-		for (int i = 1; i <=N; i++) {
-			System.out.print(NGE(i) + " ");
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= N; i++) {
+			sb.append(NGE(i) + " ");
 		}
-		
+
+		System.out.println(sb);
 	}
-	
+
 	public static int NGE(int idx) {
-		while(stack.size() > idx - 1) {
+		while (stack.size() > idx - 1) {
 			stack.pop();
 		}
 		stack.push(arr[idx - 1]);
-		
+
 		for (int i = idx; i < arr.length; i++) {
 			stack.push(arr[i]);
 			if (stack.peek() > arr[idx - 1]) {
