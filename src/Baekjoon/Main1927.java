ea255ff2e3adb6464568f6fa32afdad6b67fd3ea
khyunchoi <khyunchoi@naver.com>
2021-05-07 20:39:12 +0900

    최소힙

--- src/Baekjoon/Main1927.java
+++ src/Baekjoon/Main1927.java
@@ -1,12 +1,30 @@
 package Baekjoon;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Main1927 {
 
 	public static void main(String[] args) {
+		int N, num;
 
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		PriorityQueue<Integer> priority = new PriorityQueue<>();
+
+		while (N-- > 0) {
+			num = sc.nextInt();
+			if (num == 0) {
+				if (priority.isEmpty())
+					System.out.println("0");
+				else
+					System.out.println(priority.poll());
+			} else {
+				priority.add(num);
+			}
+		}
+		
 	}
 
 }
